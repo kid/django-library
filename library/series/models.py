6fa1ce412394b0ac81ffdf42086b019952d5f2a1
Arnaud Rebts <arnaud.rebts@gmail.com>
2008-09-24 10:34:24 +0200

    Adds method to Serie and Episode to get canonical name and path.

--- library/series/models.py
+++ library/series/models.py
@@ -1,12 +1,35 @@
+from os import path
+
 from django.db import models
 
 from common.models import ImdbItem
 
 class Serie(ImdbItem):
     short_title = models.CharField(max_length=40)
+    year = models.PositiveIntegerField()
+
+    def get_canonical_tite(self):
+        return '%s (%d)' % (self.title, self.year)
+
+    def get_path(self):
+        return path.join('Series', self.get_canonical_tite())
 
 class Episode(ImdbItem):
     serie = models.ForeignKey(Serie)
     season_number = models.PositiveSmallIntegerField()
     episode_number = models.PositiveSmallIntegerField()
-    last_episode_number = models.PositiveSmallIntegerField()
+    last_episode_number = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.get_canonical_tite()
+
+    def get_episode_number(self):
+        if self.last_episode_number:
+            return 'S%02dE%02d-%02d' % (self.season_number, self.episode_number, self.last_episode_number)
+        return 'S%02dE%02d' % (self.season_number, self.episode_number)
+
+    def get_canonical_tite(self):
+        return '%s - %s - %s' % (self.serie.get_canonical_tite(), self.get_episode_number(), self.title)
+
+    def get_path(self):
+        return path.join(self.serie.get_path(), 'Season %d' % self.season_number, self.get_canonical_tite())
