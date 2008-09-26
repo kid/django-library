aec219f6c91b1a7148ff3e7ea92bd28ed6671825
Arnaud Rebts <arnaud.rebts@gmail.com>
2008-09-26 13:47:00 +0200

    Some work on common models.

--- library/common/models.py
+++ library/common/models.py
@@ -1,4 +1,13 @@
+import os, re
+
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
+import settings
+
+def clean_filename(filename):
+    return filename.replace(' : ', ' ').replace(': ', ' ').replace(':', ' ')
 
 FORMATS = (
     (0, 'SD'),
@@ -9,18 +18,55 @@ FORMATS = (
 
 SOUND = (
     (0, 'VF'),
-    (3, 'VFC'),
-    (1, 'VO'),
-    (2, 'VF+VO'),
+    (1, 'VFC'),
+    (2, 'VO'),
+    (3, 'VF+VO'),
     (4, 'VFV+VO'),
 )
 
-class ImdbItem(models.Model):
-    title = models.CharField(max_length=1024)
-    imdb_id = models.CharField(max_length=40)
+class DatedItem(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
 
-class File(models.Model):
-    pass
+class ImdbItem(models.Model):
+    title = models.CharField(max_length=1024)
+    year = models.PositiveIntegerField(blank=True, null=True)
+    imdb_id = models.CharField(max_length=40, blank=True)
+
+    def get_canonical_tite(self):
+        return u'%s (%d)' % (self.title, self.year)
+
+    def __unicode__(self):
+        return self.get_canonical_tite()
+
+    class Meta:
+        abstract = True
+
+class File(DatedItem):
+    object_id = models.PositiveIntegerField()
+    content_type = models.ForeignKey(ContentType)
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    file_path = models.FilePathField(path=settings.LIBRARY_ROOT, match=r'^[^\.]', max_length=1024)
+
+    def __unicode__(self):
+        return u'%s' % self.file_path
+
+    def get_new_name(self):
+        ext = re.match(r'^.+\.([^.]+)$', self.file_path).group(1)
+        return u'%s.%s' % (self.content_object.get_path(), ext)
+
+    def _move(self):
+        new_name = clean_filename(self.get_new_name())
+        os.renames(
+            os.path.join(settings.LIBRARY_ROOT, self.file_path),
+            os.path.join(settings.LIBRARY_ROOT, new_name)
+        )
+        self.file_path = new_name
+
+    def save(self, force_insert=False, force_update=False):
+        if not self.pk:
+            self._move()
+        super(File, self).save(force_insert=False, force_update=False)
