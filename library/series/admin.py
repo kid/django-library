1e26e71938726e09867b363478b1fe2f6dfd24ff
Arnaud Rebts <arnaud.rebts@gmail.com>
2008-09-26 13:52:14 +0200

    Adds the admin for series models.

--- library/series/admin.py
+++ library/series/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
+from django.contrib.contenttypes import generic
 
 from models import *
+from common.models import File
+
+class FileInline(generic.GenericTabularInline):
+    model = File
+    extra = 1
 
 class SerieAdmin(admin.ModelAdmin):
     pass
 
 class EpisodeAdmin(admin.ModelAdmin):
-    pass
+    inlines = (FileInline,)
 
 admin.site.register(Serie, SerieAdmin)
 admin.site.register(Episode, EpisodeAdmin)
