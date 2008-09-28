93e15826c08f0744eddea80edeb3445c2b2c6ceb
Arnaud Rebts <arnaud.rebts@gmail.com>
2008-09-28 19:41:04 +0200

    Fix a missing import.

--- library/common/admin.py
+++ library/common/admin.py
@@ -1,6 +1,7 @@
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.generic import generic_inlineformset_factory
 
+from common.forms import FileForm
 from common.models import File
 
 class FileAdminInline(generic.GenericStackedInline):
