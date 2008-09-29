0574d84e912ab0be62c69f51a0b51008e70dd418
Arnaud Rebts <arnaud.rebts@gmail.com>
2008-09-29 13:22:09 +0200

    Activates the databrowse application.

--- library/settings.py
+++ library/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.admin',
     'django.contrib.contenttypes',
+    'django.contrib.databrowse',
     'django.contrib.sessions',
     'django.contrib.sites',
     'common',
--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls.defaults import *
-from django.contrib import admin
+from django.contrib import admin, databrowse
+
+from common.models import File
+from series.models import Episode, Serie
+
+databrowse.site.register(Serie)
+databrowse.site.register(Episode)
+databrowse.site.register(File)
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
+    (r'^databrowse/(.*)', databrowse.site.root),
 )
