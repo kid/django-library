951140c5c5548acac5e938f51fc9182433f5befa
Arnaud Rebts <arnaud.rebts@gmail.com>
2008-10-27 11:00:18 +0100

    Renamed get_episode_number to full_ep_number.

--- library/series/models.py
+++ library/series/models.py
@@ -27,13 +27,13 @@ class Episode(ImdbItem, DatedItem):
     def __unicode__(self):
         return self.get_canonical_tite()
 
-    def get_episode_number(self):
+    def full_ep_number(self):
         if self.last_episode_number:
             return u'S%02dE%02d-%02d' % (self.season_number, self.episode_number, self.last_episode_number)
         return u'S%02dE%02d' % (self.season_number, self.episode_number)
 
     def get_canonical_tite(self):
-        return u'%s - %s - %s' % (self.serie.short_title or self.serie.title, self.get_episode_number(), self.title)
+        return u'%s - %s - %s' % (self.serie.short_title or self.serie.title, self.full_ep_number(), self.title)
 
     def get_path(self):
         return path.join(self.serie.get_path(), u'Season %d' % self.season_number, self.get_canonical_tite())
